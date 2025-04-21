e0ddd11027db5450628da916095a082eee74abc0
Lom <lominism@gmail.com>
2025-04-22 01:13:43 +0700

    Remove .next directory from repository

--- .next/static/chunks/src_app_change-serial-number_page_tsx_6a37274d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_62a2de91._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_change-serial-number_page_tsx_a91fc6a2._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_change-serial-number_page_tsx_69d08c22._.js"
-],
-    source: "dynamic"
-});
